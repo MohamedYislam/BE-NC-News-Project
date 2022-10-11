69869cc9d41b6f037b654a5905a7f0c0c4e7ee93
Your Name <msyislam@gmail.com>
2022-10-11 05:02:24 +0400

    added get API for article_id

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 const express = require('express')
 const app = express();
 
-const { getTopics } = require('./controllers/controllers.js')
+const { getTopics, getArticleById } = require('./controllers/controllers.js')
 
 app.get('/api/topics', getTopics)
+app.get('/api/articles/:article_id', getArticleById)
 
 module.exports = app;
