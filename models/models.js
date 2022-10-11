9201000fe945804c7c7105cb6483bf4fd5b07986
Your Name <msyislam@gmail.com>
2022-10-11 05:02:46 +0400

    created getArticleById function and eexported

--- models/models.js
+++ models/models.js
@@ -5,3 +5,13 @@ exports.selectTopics = () => {
         return rows
     })
 }
+
+exports.selectArticleById = (article_id) => {
+    console.log("inside the models")
+    return db.query(`SELECT * FROM articles WHERE article_id = $1;`, [article_id])
+        .then(({rows : article}) => {
+            console.log(article, "<article result")
+            return article[0]
+        })
+
+}
