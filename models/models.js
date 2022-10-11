8d8d1e30bd442618c3537d15a8a8d82eeb1d0bf5
Your Name <msyislam@gmail.com>
2022-10-11 07:46:31 +0400

    added selectUsers function

--- models/models.js
+++ models/models.js
@@ -8,8 +8,11 @@ exports.selectTopics = () => {
 
 exports.selectArticleById = (article_id) => {
     return db.query(`SELECT * FROM articles WHERE article_id = $1;`, [article_id])
-        .then(({rows : article}) => {
-            return article[0]
-        })
+    .then(({rows : article}) => {
+        return article[0]
+    })
+}
 
+exports.selectUsers = () => {
+    
 }
