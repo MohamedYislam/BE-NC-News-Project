ba5a93b231785e9f1da7620698bb14eb6e7e0133
Your Name <msyislam@gmail.com>
2022-10-11 14:41:23 +0400

    added patch endpoint for article_id

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 const express = require('express')
 const app = express();
+app.use(express.json())
+const { getTopics, getArticleById, getUsers, patchArticleById } = require('./controllers/controllers.js')
 
-const { getTopics, getArticleById, getUsers } = require('./controllers/controllers.js')
-
-app.get('/api/topics', getTopics)
-app.get('/api/articles/:article_id', getArticleById)
-app.get('/api/users', getUsers)
-
+app.get('/api/topics', getTopics);
+app.get('/api/articles/:article_id', getArticleById);
+app.get('/api/users', getUsers);
+app.patch('/api/articles/:article_id', patchArticleById);
 
 module.exports = app;
