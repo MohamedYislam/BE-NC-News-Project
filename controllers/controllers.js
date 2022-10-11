009f92143d1f94967f9a50cfebe33b70177ba3ee
Your Name <msyislam@gmail.com>
2022-10-11 15:28:12 +0400

    removed console.log

--- controllers/controllers.js
+++ controllers/controllers.js
@@ -2,20 +2,19 @@ const { selectTopics, selectArticleById, selectUsers } = require('../models/mode
 
 exports.getTopics = (req, res) => {
     selectTopics().then((topics) => {
-        res.send({topics})
+        res.status(200).send({topics})
     })
 }
 
 exports.getArticleById = (req, res) => {
     const { article_id } = req.params
     selectArticleById(article_id).then((article) => {
-        res.send({article})
+        res.status(200).send({article})
     })
 }
 
 exports.getUsers = (req, res) => {
-    console.log("inside the controller")
     selectUsers().then((users) => {
-        res.send({users})
+        res.status(200).send({users})
     })    
 }
