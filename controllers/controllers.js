8ae03c26b8864719471204b0262be3dacb188397
Your Name <msyislam@gmail.com>
2022-10-11 14:52:32 +0400

    adding status 200 to response

--- controllers/controllers.js
+++ controllers/controllers.js
@@ -2,14 +2,14 @@ const { selectTopics, selectArticleById } = require('../models/models.js')
 
 exports.getTopics = (req, res) => {
     selectTopics().then((topics) => {
-        res.send({topics})
+        res.status(200).send({topics})
     })
 }
 
 exports.getArticleById = (req, res) => {
     const { article_id } = req.params
     selectArticleById(article_id).then((article) => {
-        res.send({article})
+        res.status(200).send({article})
     })
     
 }
