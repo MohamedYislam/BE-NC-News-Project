38f745786a19035cd3fc7837eeb7eaec4143b6cc
Your Name <msyislam@gmail.com>
2022-10-11 07:07:52 +0400

    removing console.logs

--- models/models.js
+++ models/models.js
@@ -7,10 +7,8 @@ exports.selectTopics = () => {
 }
 
 exports.selectArticleById = (article_id) => {
-    console.log("inside the models")
     return db.query(`SELECT * FROM articles WHERE article_id = $1;`, [article_id])
         .then(({rows : article}) => {
-            console.log(article, "<article result")
             return article[0]
         })
 
