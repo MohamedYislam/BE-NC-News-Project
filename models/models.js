6d99bc6fcb01498eff4082f4fc7924fb4b6ae996
Your Name <msyislam@gmail.com>
2022-10-12 09:04:09 +0400

    updated model to consider cases of an invalid object being sent or invalid article_id requested

--- models/models.js
+++ models/models.js
@@ -1,3 +1,4 @@
+const { ensureNoExpected } = require('jest-matcher-utils')
 const db = require('../db/connection.js')
 
 exports.selectTopics = () => {
@@ -22,12 +23,18 @@ exports.selectUsers = () => {
 }
 
 exports.updateArticleById = (article_id, inc_votes) => {
+    
+    if (!inc_votes || typeof inc_votes !== "number") {
+         return Promise.reject({ status: 400, msg: 'bad request, object sent may be invalid'});
+    }
     return db.query(`
     UPDATE articles
     SET votes = votes + $2  WHERE article_id = $1
     RETURNING *;`, [article_id, inc_votes])
     .then(({rows : updatedArticle}) => {
+        if(updatedArticle.length == 0) {
+            return Promise.reject({ status : 404, msg: 'Article does not exist'})
+        } 
         return updatedArticle[0];
     })
-
 }
