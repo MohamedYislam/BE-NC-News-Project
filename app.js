659f1eec604af574dc0ce682ae9e9ae3f9ca6901
Your Name <msyislam@gmail.com>
2022-10-12 09:01:34 +0400

    added middleware error-handling functions

--- app.js
+++ app.js
@@ -8,4 +8,20 @@ app.get('/api/articles/:article_id', getArticleById);
 app.get('/api/users', getUsers);
 app.patch('/api/articles/:article_id', patchArticleById);
 
+app.use((err, req, res , next) => {
+    if(err.status === 400) {
+        res.status(400).send({msg: err.msg})
+    } else {
+        next(err);
+    }
+})
+app.use((err, req, res, next) => {
+    if (err.status === 404){
+        res.status(404).send({msg: err.msg})
+    } else {
+        next(err)
+    }
+})
+
+
 module.exports = app;
