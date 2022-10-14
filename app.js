7ab30a25ee338ff6d6b40b0ee442ed8cdaff11f1
Your Name <msyislam@gmail.com>
2022-10-14 08:09:43 +0400

    added error handling middleware for post errors

--- app.js
+++ app.js
@@ -22,6 +22,9 @@ app.use((err, req, res, next) => {
     if (err.code === '22P02') {
         res.status(400).send({msg: 'Invalid article id'})
     }
+    if (err.code === '23503') {
+        res.status(404).send({msg: 'Article does not exist'})
+    }
     if (err.status === 404){
         res.status(404).send({msg: err.msg})
     } else {
