bcc1af6a61b6d720fe1d1a524da8714c908b71ec
Your Name <msyislam@gmail.com>
2022-10-14 14:10:08 +0400

    added POST error handling test for invalid object keys and invalid username

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -86,7 +86,7 @@ describe('GET /api/articles/:article_id', () => {
             .get(`/api/articles/77777`)
             .expect(404)
             .then(({ body: {msg} }) => {
-                expect(msg).toBe('Article does not exist');
+                expect(msg).toBe('Article not found');
             });
         })
     })
@@ -187,7 +187,7 @@ describe('PATCH /api/articles/:article_id', () => {
             .expect(404)
             .then((response) => {
                 const { msg } = response.body;
-                expect(msg).toBe('Article does not exist');
+                expect(msg).toBe('Article not found');
             });
         })
     });
@@ -336,7 +336,7 @@ describe('GET', () => {
             .get('/api/articles/77777/comments')            
             .expect(404)
             .then(({ body: {msg} }) => {
-                expect(msg).toBe('Article does not exist');
+                expect(msg).toBe('Article not found');
             });
         })
     })
@@ -370,7 +370,7 @@ describe('POST', () => {
             test('status 400 when user tries to post a comment to an invalid article_id', () =>{
                 const newComment =   {
                     username: "icellusedkars",
-                    body: "There are more stars in the heaven then grains of sand on the planet",
+                    body: "There are more stars in the heaven thann grains of sand on the planet",
                 }
                 return request(app)
                 .post('/api/articles/One/comments')
@@ -380,6 +380,19 @@ describe('POST', () => {
                     expect(msg).toBe('Invalid article id');
                 });           
             })
+            test('status 400, invalid object', () => {
+                const newComment =   {
+                    invalidKey: "icellusedkars",
+                    body: "The moon is 4.5 billion years old",
+                }
+                return request(app)
+                .post('/api/articles/1/comments')
+                .send(newComment)
+                .expect(400)
+                .then(({ body: {msg} }) => {
+                    expect(msg).toBe('Invalid object, check object key');
+                });        
+            });        
             test('status 404 when user tries to post a comment to an article which does not exist', () =>{
                 const newComment =   {
                     username: "icellusedkars",
@@ -390,8 +403,21 @@ describe('POST', () => {
                 .send(newComment)
                 .expect(404)
                 .then(({ body: {msg} }) => {
-                    expect(msg).toBe('Article does not exist');
+                    expect(msg).toBe('Article not found');
                 });           
+            })
+            test('status 404 invalid username', () => {
+                const newComment =   {
+                    username: "usernameNotinDatabase",
+                    body: "The moon is 4.5 billion years old",
+                }
+                return request(app)
+                .post('/api/articles/1/comments')
+                .send(newComment)
+                .expect(404)
+                .then(({ body: {msg} }) => {
+                    expect(msg).toBe('invalid object sent, username may not exist');
+                });        
             })
         })
 
