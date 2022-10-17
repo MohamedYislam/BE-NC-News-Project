d74b8732d6f704b49c3a5f203d33a6acfd375f15
Your Name <msyislam@gmail.com>
2022-10-17 07:07:17 +0400

    removeCommentById model function added

--- models/models.js
+++ models/models.js
@@ -119,3 +119,13 @@ exports.insertArticleIdComment = (comment, article_id, username) => {
         })
     })
 }
+
+exports.removeCommentById = (comment_id) => {
+    return db.query(`DELETE FROM comments WHERE comment_id = $1 RETURNING *;`, [comment_id])
+    .then(({rows : comments}) => {
+        if(comments.length === 0) {
+            return Promise.reject({ status : 404, msg: 'comment not found'})
+        }
+        return comments;
+    })
+}
